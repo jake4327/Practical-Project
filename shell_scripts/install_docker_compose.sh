7e8c7a495297acc6e5b2c8ef1da1f4776c1faf9c
Jake Stone <jakestone4327@gmail.com>
2020-10-06 19:54:48 +0100

    added shell scripts to install docker, docker-compose and jenkins file

--- shell_scripts/install_docker_compose.sh
+++ shell_scripts/install_docker_compose.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+sudo apt install -y curl jq
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 # download to /usr/local/bin/docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)$
