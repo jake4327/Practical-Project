f219b40680e7e2869cf20d2f1f237911b51fb5a0
Jake Stone <jakestone4327@gmail.com>
2020-10-12 20:04:00 +0100

    renamed vars in terrafom

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,18 @@
 provider "aws" {
   region = "eu-west-2"
   #version = "~> 3.10.0"
-
 }
 
 resource "aws_vpc" "main" {
 	cidr_block = "10.0.0.0/16"
 }
 
-
-resource "aws_instance" "EC2"{
+resource "aws_instance" "pytest"{
   ami = var.ami
   instance_type = var.type
 }
 
-resource "aws_instance" "EC2_1"{
+resource "aws_instance" "jenkins_server"{
   ami = var.ami
   instance_type = var.type
 }
@@ -26,20 +24,19 @@ resource "aws_db_instance" "production_db" {
  engine_version    = "5.7"
  instance_class    = "db.t2.micro"
  name              = "production_db"
- username          = "admin"
- password          = "password"
+ username          = "jake"
+ password          = "dorwssap"
  skip_final_snapshot  = true
 }
 
 resource "aws_db_instance" "test" {
-
  allocated_storage = 20
  storage_type      = "gp2"
  engine            = "mysql"
  engine_version    = "5.7"
  instance_class    = "db.t2.micro"
  name              = "test_db"
- username          = "admin"
- password          = "password"
+ username          = "testadmin"
+ password          = "test123"
  skip_final_snapshot  = true
 }
