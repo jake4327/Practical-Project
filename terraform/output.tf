40f900fd08af1c4b009d6b9423ac472485180103
Jake Stone <jakestone4327@gmail.com>
2020-10-13 07:13:54 +0100

    remvoing terraform for sercurity

--- terraform/output.tf
+++
@@ -1,4 +0,0 @@
-output "instance_ip"{
-	description = "DNS IP"
-        value = "aws_instance.default.pulic_dns"
-}
--- terraform/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "ami" {
- description = "machine image"
- default     = "ami-09a1e275e350acf38"
-}
-
-variable "type" {
- default = "t2.micro"
-}
